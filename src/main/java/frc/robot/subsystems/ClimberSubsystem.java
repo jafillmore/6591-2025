8f7d3f89b425653221cf08d2ea70eb63fa74569e
ace <ace.finnigan1120@gamil.com>
2025-02-15 20:16:04 -0800

    added ClimberSubsystem

---
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.Encoder;
+
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.RelativeEncoder;
+
+import frc.robot.Configs;
+import frc.robot.Constants;
+import frc.robot.Constants.CoralConstants;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class ClimberSubsystem extends SubsystemBase {
+  /** Creates a new Climber. */
+  public ClimberSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
